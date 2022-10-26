a58019c8915a3cac3b784c88847e948ced492805
chandrasekharSeela <seela.chandrasekhar@gmail.com>
2022-10-26 15:40:06 +0100

    with changes

--- src/com/Learning/JavaOops/MotorVechicle.java
+++ src/com/Learning/JavaOops/MotorVechicle.java
@@ -1,5 +1,43 @@
 package com.Learning.JavaOops;
 
 public class MotorVechicle {
+	
+	private final int MAXSPEED = 300;
+    private final int MINSPEED = 0;
+    private int speed;
+    
+    public int getSpeed() {
+		return speed;
+	}
 
+	public void setSpeed(int speed) {	
+		this.speed = speed > MINSPEED && speed <= MAXSPEED ? speed : speed > MAXSPEED ? MAXSPEED : MINSPEED;
+	}
+    
+     MotorVechicle() {
+    	 this(5);
+	}
+
+	public MotorVechicle(int speed) {
+		setSpeed(speed);
+	}
+    
+    public void increaseSpeed(int howMuch) {
+    	setSpeed(speed+howMuch);
+    }
+    public void decreaseSpeed(int howMuch) {
+    	setSpeed(speed - howMuch);
+    }
+    
+    public void start() {
+    	System.out.println("Started");
+    }
+    public void stop() {
+    	System.out.println("Engine Stop");
+    	speed = 0;
+    }
+	
+    public void currentSpeed() {
+    	System.out.println("Current Speed of the Vechicle is " + getSpeed());
+    }
 }
--- src/com/Learning/JavaOops/MotorVechicleRunner.java
+++ src/com/Learning/JavaOops/MotorVechicleRunner.java
@@ -4,7 +4,23 @@ public class MotorVechicleRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       MotorVechicle ducati = new MotorVechicle(10);
+       MotorVechicle honda = new MotorVechicle();
+       
+       ducati.start();
+       honda.start();
+       ducati.increaseSpeed(250);
+       honda.increaseSpeed(200);
+        
+       ducati.currentSpeed();
+       honda.currentSpeed();
+     
+       ducati.stop();
+       honda.stop();
+       
+       ducati.currentSpeed();
+       honda.currentSpeed();
+       
 	}
 
 }
